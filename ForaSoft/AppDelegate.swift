82a8fecc23b27f639ddf46ac0edcf08c895e296a
Araz Nadzhafov <brainless11.5.1994@gmail.com>
2020-12-13 18:47:43 +0300

    Start

---
+++ ForaSoft/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ForaSoft
+//
+//  Created by Наджафов Араз on 13.12.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // - UI
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("Приложение загрузилось!")
+        return true
+    }
+
+}
+
